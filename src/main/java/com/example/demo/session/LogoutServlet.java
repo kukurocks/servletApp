8bb51b022129973a24594b1a69e7745cbb815c23
kukurocks <kuksusdfgh@gmail.com>
2023-05-21 09:00:19 +0300

    add filters and session

---
+++ src/main/java/com/example/demo/session/LogoutServlet.java
@@ -0,0 +1,36 @@
+package com.example.demo.session;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("JSESSIONID")) {
+                    System.out.println("JSESSIONID=" + cookie.getValue());
+                    break;
+                }
+            }
+        }
+        HttpSession session = request.getSession(false);
+        System.out.println("User=" + session.getAttribute("user"));
+        if (session != null) {
+            session.invalidate();
+        }
+        PrintWriter out = response.getWriter();
+        out.println("Either user name or password is wrong!");
+    }
+
+}
